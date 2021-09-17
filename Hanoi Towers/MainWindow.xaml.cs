704a641598312a817ddfa01ee24e8a8792e928ef
RadioNurshat <silver2prod@gmail.com>
2021-09-17 11:38:51 +0300

    FluentWPF installed

--- Hanoi Towers/MainWindow.xaml.cs
+++ Hanoi Towers/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Hanoi_Towers
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
