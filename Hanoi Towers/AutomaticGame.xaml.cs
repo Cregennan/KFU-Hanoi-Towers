2f0c55a6a3d5b7eda6ecd2d8ebfafeed8230460c
RadioNurshat <silver2prod@gmail.com>
2021-09-24 07:40:05 +0300

    Движание и анимация перемещшения, осталось подключить к движку и доработать асинзронные анимации

---
+++ Hanoi Towers/AutomaticGame.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hanoi_Towers
+{
+    /// <summary>
+    /// Логика взаимодействия для AutomaticGame.xaml
+    /// </summary>
+    public partial class AutomaticGame : Window
+    {
+        List<Rectangle> Rings = new List<Rectangle>();
+        List<int> ColumnCenters = new List<int> { 148, 396, 644 };
+        List<int> ColumnsContainment = new List<int> { 0, 0, 0 };
+        GameSettings Settings;
+        int RingMoveTime = 5000;
+
+
+        public AutomaticGame(GameSettings gameSettings)
+        {
+            InitializeComponent();
+            Settings = gameSettings;
+            ColumnsContainment[0] = gameSettings.ringsCount;
+
+            int RingWidth = gameSettings.FirstRingWidth;
+            for(int i = 0; i<gameSettings.ringsCount; i++)
+            {
+                Rectangle rect = new Rectangle();
+                rect.Width = RingWidth;
+                rect.Height = gameSettings.ringHeight;
+
+                Canvas.SetBottom(rect,Rings.Count * gameSettings.ringHeight);
+                Canvas.SetLeft(rect, 100 - RingWidth / 2);
+
+
+
+
+                rect.Fill = (SolidColorBrush)new BrushConverter().ConvertFrom(gameSettings.RingColors[i]);
+                rect.Stroke = (SolidColorBrush)new BrushConverter().ConvertFrom(gameSettings.RingColors[i]);
+                rect.StrokeThickness = 1;
+
+                column0.Children.Add(rect);
+                Rings.Add(rect);
+                RingWidth -= gameSettings.ringWidthFall * 2;
+                
+            }
+            Point that = ((Rectangle)column0.Children[0]).TransformToAncestor(gameField).Transform(new Point(1,1));
+            Debug.WriteLine(that);
+            MoveRing(0, 1);
+        }
+
+        public void MoveRing(int from, int to)
+        {
+            Canvas fromColumn = new Canvas();
+            Canvas toColumn = new Canvas();
+
+            switch (from)
+            {
+                case 0:
+                    fromColumn = column0;
+                    break;
+                case 1:
+                    fromColumn = column1;
+                    break;
+                case 2:
+                    fromColumn = column2;
+                    break;
+                default:
+                    break;
+            }
+            switch (to)
+            {
+                case 0:
+                    toColumn = column0;
+                    break;
+                case 1:
+                    toColumn = column1;
+                    break;
+                case 2:
+                    toColumn = column2;
+                    break;
+                default:
+                    break;
+            }
+
+
+            Rectangle rect = (Rectangle)fromColumn.Children[column0.Children.Count - 1];
+
+            int CalculatedOriginLeft = (int)Canvas.GetLeft(rect) + (int)Canvas.GetLeft(fromColumn);
+            int CalculatedOriginBottom = (int)Canvas.GetBottom(rect) + (int)Canvas.GetBottom(fromColumn);
+
+            fromColumn.Children.Remove(rect);
+            Canvas.SetBottom(rect, ColumnsContainment[to] * Settings.ringHeight);
+            ColumnsContainment[from]--;
+            ColumnsContainment[to]++;
+
+            int CalculatedDestinationLeft = (int)Canvas.GetLeft(rect) + (int)Canvas.GetLeft(toColumn);
+            int CalculatedDestinationBottom = (int)Canvas.GetBottom(rect) + (int)Canvas.GetBottom(toColumn);
+
+
+            Rectangle tempRect = new Rectangle();
+
+            tempRect.Width = rect.Width;
+            tempRect.Height = rect.Height;
+            tempRect.Stroke = rect.Stroke;
+            tempRect.StrokeThickness = rect.StrokeThickness;
+            Canvas.SetLeft(tempRect, CalculatedOriginLeft);
+            Canvas.SetBottom(tempRect, CalculatedOriginBottom);
+            tempRect.Fill = rect.Fill;
+            gameField.Children.Add(tempRect);
+
+
+
+            DoubleAnimation animx = new DoubleAnimation();
+            animx.From = CalculatedOriginLeft;
+            animx.To = CalculatedDestinationLeft;
+            animx.Duration = TimeSpan.FromMilliseconds(RingMoveTime);
+            animx.Completed += (sender, e) => RingMoveCompleted(sender, e, rect, toColumn, tempRect);
+
+            DoubleAnimation animy = new DoubleAnimation();
+            animy.From = CalculatedOriginBottom;
+            animy.To = CalculatedDestinationBottom;
+            animy.Duration = TimeSpan.FromMilliseconds(RingMoveTime);
+
+
+            tempRect.BeginAnimation(Canvas.LeftProperty, animx);
+            tempRect.BeginAnimation(Canvas.BottomProperty, animy);
+            //System.Threading.Thread.Sleep(RingMoveTime);
+            //toColumn.Children.Add(rect);
+        }
+        private void RingMoveCompleted(object sender, EventArgs e, Rectangle rect, Canvas canv, Rectangle that)
+        {
+            gameField.Children.Remove(that);
+            canv.Children.Add(rect);
+        }
+    }
+}
--- Hanoi Towers/MainWindow.xaml.cs
+++ Hanoi Towers/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +24,41 @@ namespace Hanoi_Towers
         public MainWindow()
         {
             InitializeComponent();
+            Button_Click(new Window(), new RoutedEventArgs());
+
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void RingsCountSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            try
+            {
+                ringsCountField.Text = ringsCountSlider.Value.ToString();
+            }catch(Exception err)
+            {
+                Debug.WriteLine(err.Message);
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GameSettings gameSettings = new GameSettings();
+            gameSettings.gameType = gameMode.SelectedItem.ToString();
+            Debug.WriteLine(gameSettings.gameType);
+            gameSettings.ringsCount = Int32.Parse(ringsCountField.Text);
+
+
+            if (gameMode.SelectedIndex == 0)
+            {
+                AutomaticGame auto = new AutomaticGame(gameSettings);
+                this.Close();
+                auto.ShowDialog();
+            }
         }
     }
 }
